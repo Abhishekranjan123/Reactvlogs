91dea0fb109c9a25b7399bcdfcfb9763f20210a4
Abhishek <abhishek.ranjan361996@gmail.com>
2023-01-28 10:49:10 +0530

    gf

---
+++ src/Header/header.js
@@ -0,0 +1,16 @@
+function HeaderCard(){
+    return(
+        <div>
+        <div  style={{position:'absolute'}}>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/f/f9/Phoenicopterus_ruber_in_S%C3%A3o_Paulo_Zoo.jpg"  alt='...' style={{maxWidth:'100%', maxHeight:'100%'}} ></img>
+        </div>
+        <div style={{position:'absolute'}}>
+            <h1 style={{color:'white'}}>Hello</h1>
+        </div>
+        <div style={{width:'400px', height:'200px'}}>
+            
+        </div>
+        </div>
+    );
+}
+export default HeaderCard;
