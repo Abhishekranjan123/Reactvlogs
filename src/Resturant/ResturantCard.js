48db0bdd91856246dfe31cfda985b7b281a48fcf
Abhishek <abhishek.ranjan361996@gmail.com>
2023-01-28 10:51:36 +0530

    2

--- src/Resturant/ResturantCard.js
+++ src/Resturant/ResturantCard.js
@@ -11,7 +11,7 @@ function ResturantCard(){
         <div class="card"  style={{width: "18rem"}}>
         <img src="https://upload.wikimedia.org/wikipedia/commons/f/f9/Phoenicopterus_ruber_in_S%C3%A3o_Paulo_Zoo.jpg" class="card-img-top" alt="..."></img>
         <div class="card-body">
-          <h5 class="card-text" style={{textAlign: "center"}}>Punjabi Hotel</h5>
+          <h4 class="card-text" style={{textAlign: "center"}}>Punjabi Hotel</h4>
           <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
         </div>
       </div>
